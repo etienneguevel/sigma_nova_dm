0a5ed48b30f2df1a881e463c8a47dee55faaa938
etienneguevel <etienneguev@gmail.com>
2026-07-06 09:05:42 +0200

    feat: add simple data viz to compare cbramod / simpleconv

--- scripts/unzip_file.py
+++ scripts/unzip_file.py
@@ -1,9 +1,26 @@
+import sys
+
 import pyzipper
 
 def main():
-    pwd = input("Please type the password to unzip the downloaded file:\n")
-    with pyzipper.AESZipFile("36728994") as zf:
-        zf.extractall(pwd=bytes(pwd, "utf-8"))
+    num_tries = 0
+    while (num_tries < 3):
+        try:
+            pwd = input("Please type the password to unzip the downloaded file:\n")
+            with pyzipper.AESZipFile("36728994") as zf:
+                zf.extractall(pwd=bytes(pwd, "utf-8"))
+
+        except:
+            print("\nThe indicated password is wrong, try again.")
+            num_tries += 1
+
+        else:
+            print("Extraction successful.")
+            break
+
+    else:
+        print("Too many failed attempts. Exiting.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
